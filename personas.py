281637a06cdc0e3c2ab53c715baa620e41f907c4
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-05-29 11:44:32 +0200

    clase_personas

---
+++ personas.py
@@ -0,0 +1,9 @@
+class Personas():
+    def __init__(self,id=None, 
+                name=None, 
+                sex=None, 
+                age=None) -> None:
+        self.id = id
+        self.name = name
+        self.age = age
+        self.sex = sex
